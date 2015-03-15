c730d98b91cc052375bd2e689fef980db05a6a7d
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-03-15 12:02:32 -0400

    Changed repo name

---
+++ src/MLKernels.jl
@@ -0,0 +1,46 @@
+#===================================================================================================
+  Kernel Functions Module
+===================================================================================================#
+
+module KernelFunctions
+
+import Base: show, call, exp, eltype, isposdef, convert, promote
+
+export
+    # Functions
+    arguments,
+    description,
+    isposdef_kernel,
+    kernel_function,
+    kernel_matrix,
+    description_string,
+    gramian_matrix,
+    lagged_gramian_matrix,
+    center_kernel_matrix!,
+    center_kernel_matrix,
+    # Types
+    Kernel,
+        SimpleKernel,
+            StandardKernel,
+                # PointwiseProductKernel,
+                EuclideanDistanceKernel,
+	                GaussianKernel,
+	                LaplacianKernel,
+            	    RationalQuadraticKernel,
+            	    MultiQuadraticKernel,
+            	    InverseMultiQuadraticKernel,
+            	    PowerKernel,
+            	    LogKernel,
+                ScalarProductKernel,
+	                LinearKernel,
+	                PolynomialKernel,
+            	    SigmoidKernel,
+            ScaledKernel,
+        CompositeKernel,
+            KernelProduct,
+            KernelSum
+
+include("kerneltypes.jl")
+include("kernelmatrix.jl")
+
+end # KernelFunctions
