580fe8813b7fef959bb9ca93da1cf5049b8e2c38
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-03-21 20:43:08 -0400

    Expanded test cases

--- test/test_kerneltypes.jl
+++ test/test_kerneltypes.jl
@@ -6,21 +6,66 @@ for T in (Float32, Float64)
     x, y = [one(T)], [one(T)]
 
     kernel = LinearKernel(one(T))
-    s2kernel = ScaledKernel{T}(convert(T, 2),kernel)
-    @test show(s2kernel) == Nothing()
-    s4kernel = ScaledKernel{T}(convert(T, 4),kernel)
+    skernel = ScaledKernel{T}(convert(T, 2),kernel)
+
+    @test skernel.a == convert(T,2)
+    @test skernel.κ == kernel
+
+    @test show(skernel) == Nothing()
+    println()
 
     for S in (Float32, Float64, Int32, Int64)
-        @test ScaledKernel(convert(T,2), kernel).a == s2kernel.a
-        @test *(convert(T,2), kernel).a == s2kernel.a
-        @test *(kernel, convert(T,2)).a == s2kernel.a
-        @test *(convert(T,2), s2kernel).a == s4kernel.a
-        @test *(s2kernel, convert(T,2)).a == s4kernel.a
+        @test ScaledKernel(convert(S,2), kernel).a == convert(T, 2)
+        @test *(convert(S,2), kernel).a == convert(T, 2)
+        @test *(kernel, convert(S,2)).a == convert(T, 2)
+        @test *(convert(S,2), skernel).a == convert(T, 4)
+        @test *(skernel, convert(S,2)).a == convert(T, 4)
     end
 
-    @test kernel_function(s2kernel, x ,y) == convert(T, 4)
-    @test isposdef_kernel(s2kernel) == true
+    @test kernel_function(skernel, x ,y) == convert(T, 4)
+    @test isposdef_kernel(skernel) == true
 
 end
+
+for T in (Float32, Float64)
+    x, y = [one(T)], [one(T)]
+
+    kernel = LinearKernel(one(T))
+    skernel = ScaledKernel(convert(T,2), kernel)
+    kernelprod = KernelProduct{T}(convert(T, 2),kernel, LinearKernel(zero(T)))
+
+    @test kernelprod.a == convert(T,2)
+    @test kernelprod.κ₁ == kernel
+    @test kernelprod.κ₂ == LinearKernel(zero(T))
+
+    @test show(kernelprod) == Nothing()
+    println()
+    
+    @test *(kernel, LinearKernel(1.0)).a == 1.0
+
+    for S in (Float32, Float64, Int32, Int64)
+        
+        @test *(convert(S,2), kernel, kernel).a == convert(T, 2)
+        @test *(kernel, kernel, convert(S,2)).a == convert(T, 2)
+        @test *(kernel, convert(S,2), kernel).a == convert(T, 2)
+
+        @test *(convert(S,2), skernel, kernel).a == convert(T, 4)
+        @test *(kernel, skernel, convert(S,2)).a == convert(T, 4)
+        @test *(kernel, convert(S,2), skernel).a == convert(T, 4)
+
+        @test *(convert(S,2), skernel, skernel).a == convert(T, 8)
+        @test *(skernel, skernel, convert(S,2)).a == convert(T, 8)
+        @test *(skernel, convert(S,2), skernel).a == convert(T, 8)
+
+        @test *(kernelprod, convert(S,2)).a == convert(T, 4)
+        @test *(convert(S,2), kernelprod).a == convert(T, 4)
+
+    end
+
+    @test kernel_function(kernelprod, x ,y) == convert(T, 4)
+    @test isposdef_kernel(kernelprod) == true
+
+end
+
 
 
