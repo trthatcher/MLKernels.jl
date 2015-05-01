f04537a53c8e595a29046ac9343365722e0a9f27
ST John <stj26@cam.ac.uk>
2015-05-01 09:51:46 +0100

    formatting

--- src/vectorfunctions.jl
+++ src/vectorfunctions.jl
@@ -20,6 +20,7 @@ end
 
 dscprod_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = y
 dscprod_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = x
+
 function dscprod_dx!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T})
     (n = length(x)) == length(y) == length(w) || throw(ArgumentError("Dimensions do not conform."))
     @inbounds @simd for i = 1:n
@@ -27,14 +28,16 @@ function dscprod_dx!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T})
     end
     x
 end
+
 dscprod_dy!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dscprod_dx!(y, x, w)
 dscprod_dw!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dscprod_dx!(w, x, y)
+
 dscprod_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dscprod_dx!(similar(x), y, w)
 dscprod_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dscprod_dy!(x, similar(y), w)
 dscprod_dw{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dscprod_dy!(x, y, similar(w))
 
 
-# Euclidean Product / Squared Norm Kernels
+# Euclidean Distance Kernels
  
 function sqdist{T<:FloatingPoint}(x::Array{T}, y::Array{T})
     (n = length(x)) == length(y) || throw(ArgumentError("Dimensions do not conform."))
@@ -58,21 +61,25 @@ end
 
 dsqdist_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = 2(x - y)
 dsqdist_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = 2(y - x)
+
 function dsqdist_dx!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T})
     (n = length(x)) == length(y) == length(w) || throw(ArgumentError("Dimensions do not conform."))
     @inbounds @simd for i = 1:n
-        x[i] = 2(x[i] - y[i])*w[i]^2
+        x[i] = 2(x[i] - y[i]) * w[i]^2
     end
     x
 end
+
 dsqdist_dy!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dsqdist_dx!(y, x, w)
+
 function dsqdist_dw!{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T})
     (n = length(x)) == length(y) == length(w) || throw(ArgumentError("Dimensions do not conform."))
     @inbounds @simd for i = 1:n
-        w[i] = 2(x[i] - y[i])^2*w[i]
+        w[i] = 2(x[i] - y[i])^2 * w[i]
     end
     w
 end
+
 dsqdist_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dsqdist_dx!(copy(x), y, w)
 dsqdist_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dsqdist_dy!(x, copy(y), w)
 dsqdist_dw{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = dsqdist_dw!(x, y, copy(w))
