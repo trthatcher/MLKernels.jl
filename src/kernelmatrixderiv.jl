b6b6565666673dd3cf365594324776503fd8af16
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-05-09 20:38:01 -0400

    Refactored code for readability

--- src/kernelmatrixderiv.jl
+++ src/kernelmatrixderiv.jl
@@ -14,11 +14,9 @@ function kernelmatrix_dx{T<:FloatingPoint}(κ::StandardKernel{T}, X::Matrix{T}, 
         throw(ArgumentError("X and Y do not have the same number of " * (is_trans ? "rows." : "columns.")))
     end
     K = Array(T, d, n, m)
-    @transpose_access is_trans (X,Y) @inbounds for j = 1:m 
-            for i = 1:n
-                K[:,i,j] = kernel_dx(κ, X[i,:], Y[j,:])
-            end
-        end
+    @transpose_access is_trans (X,Y) @inbounds for j = 1:m, i = 1:n
+        K[:,i,j] = kernel_dx(κ, X[i,:], Y[j,:])
+    end
     reshape(K, (d*n, m))
 end
 
@@ -30,11 +28,9 @@ function kernelmatrix_dy{T<:FloatingPoint}(κ::StandardKernel{T}, X::Matrix{T}, 
         throw(ArgumentError("X and Y do not have the same number of " * (is_trans ? "rows." : "columns.")))
     end
     K = Array(T, n, d, m)
-    @transpose_access is_trans (X,Y) @inbounds for j = 1:m 
-            for i = 1:n
-                K[i,:,j] = kernel_dy(κ, X[i,:], Y[j,:])
-            end
-        end
+    @transpose_access is_trans (X,Y) @inbounds for j = 1:m, i = 1:n
+        K[i,:,j] = kernel_dy(κ, X[i,:], Y[j,:])
+    end
     reshape(K, (n, d*m))
 end
 
@@ -46,52 +42,27 @@ function kernelmatrix_dxdy{T<:FloatingPoint}(κ::StandardKernel{T}, X::Matrix{T}
         throw(ArgumentError("X and Y do not have the same number of " * (is_trans ? "rows." : "columns.")))
     end
     K = Array(T, d, n, d, m)
-    if trans == 'N'
-        @inbounds for j = 1:m, i = 1:n
-            N_kernel_dxdy!(κ, d, K, X, i, Y, j)
-        end
-    else
-        @inbounds for j = 1:m, i = 1:n
-            T_kernel_dxdy!(κ, d, K, X, i, Y, j)
-        end
+    @inbounds for j = 1:m, i = 1:n
+        kernel_dxdy!(κ, d, K, X, i, Y, j, is_trans)
     end
     K # reshape(K, (d*n, d*m))
 end
+
 
 #==========================================================================
   Optimized Kernel Derivative Matrices for Squared Distance Kernels
 ==========================================================================#
 
-for (TN_kernel_dxdy!, TN_sqdist, is_T) in ((:N_kernel_dxdy!, :N_sqdist, false), 
-                                           (:T_kernel_dxdy!, :T_sqdist, true))
-    @eval begin
-
-        # Squared distance between vectors X[x_pos,:] and Y[y_pos,:] (X[:,x_pos] and Y[:,y_pos])
-        function $TN_sqdist{T<:FloatingPoint}(d::Int64, X::Array{T}, x_pos::Int64, Y::Array{T}, y_pos::Int64)
-            z = zero(T)
-            @inbounds @simd for i = 1:d
-                v = $(is_T ? :(X[i,x_pos] - Y[i,y_pos]) : :(X[x_pos,i] - Y[y_pos,i]))
-                z += v*v
-            end
-            z
+function kernel_dxdy!{T<:FloatingPoint}(κ::SquaredDistanceKernel{T}, d::Int64, K::Array{T}, X::Array{T}, x_pos::Int64, Y::Array{T}, y_pos::Int64, is_trans::Bool)
+    ϵᵀϵ = sqdist(d, X, x_pos, Y, y_pos, is_trans)
+    ∂κ∂z = kappa_dz(κ, ϵᵀϵ)
+    ∂κ²∂z² = kappa_dz2(κ, ϵᵀϵ)
+    @transpose_access is_trans (X,Y) @inbounds for j = 1:d
+        v = X[x_pos,j] - Y[y_pos,j]
+        for i = 1:d
+            K[i,x_pos,j,y_pos] = -4∂κ²∂z² * v *  X[x_pos,i] - Y[y_pos,i]
         end
-
-        # In-place update of K[x_pos,:,y_pos,:] (K[:,x_pos,:,y_pos]) to contain ∂k²/∂x∂y
-        function $TN_kernel_dxdy!{T<:FloatingPoint}(κ::SquaredDistanceKernel{T}, d::Int64, K::Array{T}, X::Array{T}, x_pos::Int64, Y::Array{T}, y_pos::Int64)
-            ϵᵀϵ = $TN_sqdist(d, X, x_pos, Y, y_pos)
-            ∂κ∂z = kappa_dz(κ, ϵᵀϵ)
-            ∂κ²∂z² = kappa_dz2(κ, ϵᵀϵ)
-            @inbounds for j = 1:d
-                v = $(is_T ? :(X[j,x_pos] - Y[j,y_pos]) : :(X[x_pos,j] - Y[y_pos,j]))
-                for i = 1:d
-                    K[i,x_pos,j,y_pos] = -4∂κ²∂z² * v *  $(is_T ? :(X[i,x_pos] - Y[i,y_pos]) : :(X[x_pos,i] - Y[y_pos,i]))
-                end
-                K[j,x_pos,j,y_pos] -= 2∂κ∂z
-            end
-            K
-        end
-
+        K[j,x_pos,j,y_pos] -= 2∂κ∂z
     end
+    K
 end
-
-
--- src/vectorfunctions.jl
+++ src/vectorfunctions.jl
@@ -96,10 +96,20 @@ function sqdist{T<:FloatingPoint}(x::Array{T}, y::Array{T})
     z
 end
 
+# In-place squared distance calculation
+function sqdist{T<:FloatingPoint}(d::Int64, X::Array{T}, x_pos::Int64, Y::Array{T}, y_pos::Int64, is_trans::Bool)
+    z = zero(T)
+    @transpose_access is_trans (X,Y) @inbounds for i = 1:d
+        v = X[x_pos,i] - Y[y_pos,i]
+        z += v*v
+    end
+    z
+end
+
 sqdist_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = scale!(2, x - y)
 sqdist_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}) = scale!(2, y - x)
 
-# Calculates G such that Gij is the dot product of the difference of row i and j of matrix X
+# Calculates Z such that Zij is the dot product of the difference of row i and j of matrix X
 #    trans == 'N' -> X is a design matrix
 #             'T' -> X is a transposed design matrix
 function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, trans::Char = 'N', uplo::Char = 'U', sym::Bool = true)
@@ -114,7 +124,7 @@ function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, trans::Char = 'N', uplo::C
     sym ? (uplo == 'U' ? syml!(Z) : symu!(Z)) : Z
 end
 
-# Calculates the upper right corner G of the squared distance matrix of matrix [Xᵀ Yᵀ]ᵀ
+# Calculates the upper right corner, Z, of the squared distance matrix of matrix [Xᵀ Yᵀ]ᵀ
 #   trans == 'N' -> X and Y are design matrices
 #            'T' -> X and Y are transposed design matrices
 function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, Y::Matrix{T}, trans::Char = 'N')
@@ -169,7 +179,7 @@ sqdist_dx{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = sqdist_dx!(
 sqdist_dy{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = sqdist_dy!(x, copy(y), w)
 sqdist_dw{T<:FloatingPoint}(x::Array{T}, y::Array{T}, w::Array{T}) = sqdist_dw!(x, y, copy(w))
 
-# Calculates G such that Gij is the dot product of the difference of row i and j of matrix X
+# Calculates Z such that Zij is the dot product of the difference of row i and j of matrix X
 #    trans == 'N' -> X is a design matrix
 #             'T' -> X is a transposed design matrix
 function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, w::Array{T}, trans::Char = 'N', uplo::Char = 'U', sym::Bool = true)
@@ -184,7 +194,7 @@ function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, w::Array{T}, trans::Char =
     sym ? (uplo == 'U' ? syml!(Z) : symu!(Z)) : Z
 end
 
-# Calculates the upper right corner G of the squared distance matrix of matrix [Xᵀ Yᵀ]ᵀ
+# Calculates the upper right corner, Z, of the squared distance matrix of matrix [Xᵀ Yᵀ]ᵀ
 #   trans == 'N' -> X and Y are design matrices
 #            'T' -> X and Y are transposed design matrices
 function sqdistmatrix{T<:FloatingPoint}(X::Matrix{T}, Y::Matrix{T}, w::Array{T}, trans::Char = 'N')
