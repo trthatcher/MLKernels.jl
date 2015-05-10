35bbe9fc6dcacd7a5a7ba1c0e735bef897055017
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-05-10 12:24:16 -0400

    Expanded test cases

--- test/test_matrixfunctions.jl
+++ test/test_matrixfunctions.jl
@@ -15,6 +15,9 @@ for T in (Float32,Float64)
            7 8 9]
     dr = T[1*1+2*2+3*3, 77, 194]
     dc = T[1*1+4*4+7*7, 93, 126]
+    w = T[1;2;3]
+    wdr = vec(sum((S1 .* S1) .* w',2))
+    wdc = vec(sum((S1 .* S1) .* w,1))
     row = T[11 12 13]
     col = T[11;12;13]
     RA = T[12 14 16;
@@ -34,6 +37,8 @@ for T in (Float32,Float64)
     @test MLKernels.symu(S1) == SU
     @test MLKernels.dot_rows(S1) == dr
     @test MLKernels.dot_columns(S1) == dc
+    @test MLKernels.dot_rows(S1,w) == wdr
+    @test MLKernels.dot_columns(S1,w) == wdc
     @test MLKernels.row_add!(copy(S1), row) == RA
     @test MLKernels.col_add!(copy(S1), col) == CA
     @test MLKernels.row_sub!(copy(S1), row) == RS
