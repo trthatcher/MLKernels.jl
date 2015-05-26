36cd410f12872f74fb5631acb0e2f96f03808edc
ST John <stj26@cam.ac.uk>
2015-05-26 14:22:41 +0100

    Add test for nystrom kernel matrix approximation

---
+++ test/test_approx.jl
@@ -0,0 +1,18 @@
+using Base.Test
+
+importall MLKernels
+
+function basenystrom(kernel::Kernel, X::Matrix, xs::Vector)
+    C = kernelmatrix(kernel, X, X[xs,:])
+    D = C[xs,:]
+    SVD = svdfact(D)
+    DVC = diagm(1./sqrt(SVD[:S])) * SVD[:Vt] * C'
+    MLKernels.syml(BLAS.syrk('U', 'T', 1, DVC))
+end
+
+print("- Testing kernel matrix approximation ... ")
+k = SquaredExponentialKernel()
+X = rand(5,3)
+@test_approx_eq nystrom(k, X, [1,3,5]) basenystrom(k, X, [1,3,5])
+@test_approx_eq nystrom(k, X, [1:5]) kernelmatrix(k, X)
+println("Done")
