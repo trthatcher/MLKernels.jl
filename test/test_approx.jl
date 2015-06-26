e736aa3c2a1581d2f706d533f94e3bd64461c937
ST John <stj26@cam.ac.uk>
2015-06-26 11:31:33 +0100

    Fix sporadic test error (due to division by zero)

--- test/test_approx.jl
+++ test/test_approx.jl
@@ -6,7 +6,9 @@ function basenystrom(kernel::Kernel, X::Matrix, xs::Vector)
     C = kernelmatrix(kernel, X, X[xs,:])
     D = C[xs,:]
     SVD = svdfact(D)
-    DVC = diagm(1./sqrt(SVD[:S])) * SVD[:Vt] * C'
+    S = 1 ./ sqrt(SVD[:S])
+    S[abs(SVD[:S]) .< 1e-10] = 0 # remove infinities
+    DVC = diagm(S) * SVD[:Vt] * C'
     MLKernels.syml(BLAS.syrk('U', 'T', 1, DVC))
 end
 
