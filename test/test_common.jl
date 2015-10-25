eb02d4e22a878c66f3493a53cfa29b541c782b5e
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-10-24 23:51:43 -0400

    Expanded tests for matrix_sum! and matrix_prod

--- test/test_common.jl
+++ test/test_common.jl
@@ -7,6 +7,12 @@ AL = [1 2 3;
 AU = [1 4 7;
       4 5 8;
       7 8 9]
+U = [3 3 3;
+     0 3 3;
+     0 0 3]
+L = [3 0 0;
+     3 3 0;
+     3 3 3]
 
 dotc = sum(A .* A, 1)
 dotr = sum(A .* A, 2)
@@ -37,11 +43,20 @@ end
 info("Testing ", MLKernels.matrix_prod!)
 for T in FloatingPointTypes
     @test MLKernels.matrix_prod!(convert(Array{T},A), convert(Array{T},A)) == convert(Array{T}, A .* A)
+    @test MLKernels.matrix_prod!(convert(Array{T},U), convert(Array{T},U), true, false) == 3*convert(Array{T},U)
+    @test MLKernels.matrix_prod!(convert(Array{T},U), convert(Array{T},U), true, true)  == 9*ones(T, size(U)...)
+    @test MLKernels.matrix_prod!(convert(Array{T},L), convert(Array{T},L), false, false) == 3*convert(Array{T},L)
+    @test MLKernels.matrix_prod!(convert(Array{T},L), convert(Array{T},L), false, true)  == 9*ones(T, size(L)...)
 end
 
 info("Testing ", MLKernels.matrix_sum!)
 for T in FloatingPointTypes
     @test MLKernels.matrix_sum!(convert(Array{T},A), convert(Array{T},A)) == convert(Array{T}, A .+ A)
+    @test MLKernels.matrix_sum!(convert(Array{T},U), convert(Array{T},U), true, false) == 2*convert(Array{T},U)
+    @test MLKernels.matrix_sum!(convert(Array{T},U), convert(Array{T},U), true, true)  == 6*ones(T, size(U)...)
+    @test MLKernels.matrix_sum!(convert(Array{T},L), convert(Array{T},L), false, false) == 2*convert(Array{T},L)
+    @test MLKernels.matrix_sum!(convert(Array{T},L), convert(Array{T},L), false, true)  == 6*ones(T, size(L)...)
+
 end
 
 info("Testing ", MLKernels.translate!)
