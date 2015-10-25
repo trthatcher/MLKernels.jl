4eda0cca93b03aa9fcfcffca703e2ba6a789a262
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-10-25 16:42:32 -0400

    Refactored test code

--- test/reference.jl
+++ test/reference.jl
@@ -2,6 +2,7 @@ FloatingPointTypes = (Float32, Float64, BigFloat)
 
 # Samples Input
 
+#=
 x1 = [1; 2]
 x2 = [2; 0]
 x3 = [3; 2]
@@ -18,6 +19,7 @@ w = [2; 1]
 
 Set_x = (x1,x2,x3)
 Set_y = (y1,y2)
+=#
 
 # Additive Kernel References
 
@@ -28,73 +30,6 @@ additive_kernels = (
     ScalarProductKernel,
     MercerSigmoidKernel
 )
-
-additive_kernelfunctions = Dict(
-    SquaredDistanceKernel => (t,x,y) -> ((x-y)^2)^t,
-    SineSquaredKernel => (t,x,y) -> (sin(x-y)^2)^t,
-    ChiSquaredKernel => (t,x,y) -> (x == y == 0) ? zero(typeof(t)) : ((x-y)^2/(x+y))^t,
-    ScalarProductKernel => (x,y) -> x*y,
-    MercerSigmoidKernel => (d,b,x,y) -> tanh((x-d)/b) * tanh((y-d)/b)
-)
-
-additive_kernelranges = Dict(
-    SquaredDistanceKernel => (:Rp, true, false, true, false, false), # (range,attainszero,pos,nonneg,nonpos,neg)
-    SineSquaredKernel => (:Rp, true, false, true, false, false),
-    ChiSquaredKernel => (:Rp, true, false, true, false, false),
-    ScalarProductKernel => (:R, true, false, false, false, false),
-    MercerSigmoidKernel => (:R, true, false, false, false, false)
-)
-
-additive_testinputs = Dict(
-    SquaredDistanceKernel => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
-    SineSquaredKernel => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
-    ChiSquaredKernel => ([1,1],[1,0],[0,1],[0,0]),
-    ScalarProductKernel => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
-    MercerSigmoidKernel => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1])
-)
-
-additive_kernelargs = Dict(
-    SquaredDistanceKernel => ([:t],[1],[0.5]),
-    SineSquaredKernel => ([:t],[1],[0.5]),
-    ChiSquaredKernel => ([:t],[1],[0.5]),
-    ScalarProductKernel => (Symbol[], Int[], Int[]),
-    MercerSigmoidKernel => ([:d,:b],[0,1],[0.5,2])
-)
-
-additive_ismercer = Dict(
-    SquaredDistanceKernel => false,
-    SineSquaredKernel => false,
-    ChiSquaredKernel => false,
-    ScalarProductKernel => true,
-    MercerSigmoidKernel => true
-)
-
-additive_isnegdef = Dict(
-    SquaredDistanceKernel => true,
-    SineSquaredKernel => true,
-    ChiSquaredKernel => true,
-    ScalarProductKernel => false,
-    MercerSigmoidKernel => false
-)
-
-additive_cases = Dict(
-    SquaredDistanceKernel => ([0.25],[0.5],[1]),
-    SineSquaredKernel => ([0.25],[0.5],[1]),
-    ChiSquaredKernel => ([0.25],[0.5],[1]),
-    ScalarProductKernel => (Int[],),
-    MercerSigmoidKernel => ([0.5,2],[0,1])
-)
-
-additive_errorcases = Dict(
-    SquaredDistanceKernel => ([1.1], [0]),
-    SineSquaredKernel => ([1.1], [0]),
-    ChiSquaredKernel => ([1.1], [0]),
-    MercerSigmoidKernel => ([0,0],)
-)
-
-
-
-# Composite Kernel Cases
 
 composite_kernels = (
     ExponentialKernel,
@@ -108,104 +43,114 @@ composite_kernels = (
 )
 
 composite_pairs = Dict(
-    ExponentialKernel => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
+    ExponentialKernel       => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
     RationalQuadraticKernel => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
-    MaternKernel => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
-    PowerKernel => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
-    LogKernel => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
-    PolynomialKernel => (ScalarProductKernel,MercerSigmoidKernel),
-    ExponentiatedKernel => (ScalarProductKernel,MercerSigmoidKernel),
-    SigmoidKernel => (ScalarProductKernel,MercerSigmoidKernel)
+    MaternKernel            => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
+    PowerKernel             => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
+    LogKernel               => (SquaredDistanceKernel,SineSquaredKernel,ChiSquaredKernel),
+    PolynomialKernel        => (ScalarProductKernel,MercerSigmoidKernel),
+    ExponentiatedKernel     => (ScalarProductKernel,MercerSigmoidKernel),
+    SigmoidKernel           => (ScalarProductKernel,MercerSigmoidKernel)
 )
 
-composite_kernelfunctions = Dict(
-    ExponentialKernel => (α,γ,z) -> exp(-α*z^γ),
-    RationalQuadraticKernel => (α,β,γ,z) -> (1 + α*z^γ)^(-β),
-    MaternKernel => (ν,θ,z) -> begin 
-                                   v1 = sqrt(2*ν) * z / θ
-                                   v1 = v1 < eps(typeof(z)) ? eps(typeof(z)) : v1
-                                   2*(v1/2)^ν * besselk(ν,v1)/gamma(ν)
-                               end,
-    PowerKernel => (γ,z) -> z^γ,
-    LogKernel => (α,γ,z) -> log(α*z^γ+1),
-    PolynomialKernel => (α,c,d,z) -> (α*z+c)^d,
-    ExponentiatedKernel => (α,z) -> exp(α*z),
-    SigmoidKernel => (α,c,z) -> tanh(α*z+c)
-)
-
-composite_kernelranges = Dict(
-    ExponentialKernel => (0, false, 1, true), # (min, attains_min, max, attains_max)
-    RationalQuadraticKernel => (0, false, 1, true),
-    MaternKernel => (0, false, 1, true),
-    PowerKernel => (0, true, Inf, true),
-    LogKernel => (0, true, Inf, true),
-    PolynomialKernel => (-Inf, true, Inf, true),
-    ExponentiatedKernel => (0, false, Inf, true),
-    SigmoidKernel => (-Inf, true, Inf, true)
-)
-
-composite_testinputs = Dict(
-    ExponentialKernel => ([0],[1e-10],[0.5],[1]),
-    RationalQuadraticKernel => ([0],[1e-10],[0.5],[1]),
-    MaternKernel => ([0],[1e-10],[0.5],[1]),
-    PowerKernel => ([0],[1e-10],[0.5],[1]),
-    LogKernel => ([0],[1e-10],[0.5],[1]),
-    PolynomialKernel => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1]),
-    ExponentiatedKernel => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1]),
-    SigmoidKernel => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1])
-)
-
-composite_kernelargs = Dict(
-    ExponentialKernel => ([:alpha, :gamma], [1,1], [2,0.5]),
+all_kernelargs = Dict(
+    SquaredDistanceKernel   => ([:t],[1],[0.5]),
+    SineSquaredKernel       => ([:t],[1],[0.5]),
+    ChiSquaredKernel        => ([:t],[1],[0.5]),
+    ScalarProductKernel     => (Symbol[], Int[], Int[]),
+    MercerSigmoidKernel     => ([:d,:b],[0,1],[0.5,2]),
+    ExponentialKernel       => ([:alpha, :gamma], [1,1], [2,0.5]),
     RationalQuadraticKernel => ([:alpha, :beta, :gamma], [1,1,1], [2,2,0.5]),
-    MaternKernel => ([:nu, :theta], [1,1], [2,2]),
-    PowerKernel => ([:gamma], [1], [0.5]),
-    LogKernel => ([:alpha, :gamma], [1,1], [2,0.5]),
-    PolynomialKernel => ([:alpha, :c, :d], [1,1,2], [2,2,3]),
-    ExponentiatedKernel => ([:alpha], [1], [2]),
-    SigmoidKernel => ([:alpha, :c], [1, 1], [2, 2])
+    MaternKernel            => ([:nu, :theta], [1,1], [2,2]),
+    PowerKernel             => ([:gamma], [1], [0.5]),
+    LogKernel               => ([:alpha, :gamma], [1,1], [2,0.5]),
+    PolynomialKernel        => ([:alpha, :c, :d], [1,1,2], [2,2,3]),
+    ExponentiatedKernel     => ([:alpha], [1], [2]),
+    SigmoidKernel           => ([:alpha, :c], [1, 1], [2, 2])
 )
 
-composite_ismercer = Dict(
-    ExponentialKernel => true,
-    RationalQuadraticKernel => true,
-    MaternKernel => true,
-    PowerKernel => false,
-    LogKernel => false,
-    PolynomialKernel => true,
-    ExponentiatedKernel => true,
-    SigmoidKernel => false
+all_kernelfunctions = Dict(
+    SquaredDistanceKernel   => (t,x,y)   -> ((x-y)^2)^t,
+    SineSquaredKernel       => (t,x,y)   -> (sin(x-y)^2)^t,
+    ChiSquaredKernel        => (t,x,y)   -> (x == y == 0) ? zero(typeof(t)) : ((x-y)^2/(x+y))^t,
+    ScalarProductKernel     => (x,y)     -> x*y,
+    MercerSigmoidKernel     => (d,b,x,y) -> tanh((x-d)/b) * tanh((y-d)/b),
+    ExponentialKernel       => (α,γ,z)   -> exp(-α*z^γ),
+    RationalQuadraticKernel => (α,β,γ,z) -> (1 + α*z^γ)^(-β),
+    MaternKernel            => (ν,θ,z) -> begin 
+                                              v1 = sqrt(2*ν) * z / θ
+                                              v1 = v1 < eps(typeof(z)) ? eps(typeof(z)) : v1
+                                              2*(v1/2)^ν * besselk(ν,v1)/gamma(ν)
+                                          end,
+    PowerKernel             => (γ,z)     -> z^γ,
+    LogKernel               => (α,γ,z)   -> log(α*z^γ+1),
+    PolynomialKernel        => (α,c,d,z) -> (α*z+c)^d,
+    ExponentiatedKernel     => (α,z)     -> exp(α*z),
+    SigmoidKernel           => (α,c,z)   -> tanh(α*z+c)
+
 )
 
-composite_isnegdef = Dict(
-    ExponentialKernel => false,
-    RationalQuadraticKernel => false,
-    MaternKernel => false,
-    PowerKernel => true,
-    LogKernel => true,
-    PolynomialKernel => false,
-    ExponentiatedKernel => false,
-    SigmoidKernel => false
+all_kernelproperties = Dict(  # (range,attainszero,pos,nonneg,nonpos,neg,ismercer,isnegdef)
+    SquaredDistanceKernel   => (:Rp, true,  false, true,  false, false, false, true), 
+    SineSquaredKernel       => (:Rp, true,  false, true,  false, false, false, true),
+    ChiSquaredKernel        => (:Rp, true,  false, true,  false, false, false, true),
+    ScalarProductKernel     => (:R,  true,  false, false, false, false, true,  false),
+    MercerSigmoidKernel     => (:R,  true,  false, false, false, false, true,  false),
+    ExponentialKernel       => (:Rp, false, true,  true,  false, false, true,  false),
+    RationalQuadraticKernel => (:Rp, false, true,  true,  false, false, true,  false),
+    MaternKernel            => (:Rp, false, true,  true,  false, false, true,  false),
+    PowerKernel             => (:Rp, true,  false, true,  false, false, false, true),
+    LogKernel               => (:Rp, true,  false, true,  false, false, false, true),
+    PolynomialKernel        => (:R,  true,  false, false, false, false, true,  false),
+    ExponentiatedKernel     => (:Rp, false, true,  true,  false, false, true,  false),
+    SigmoidKernel           => (:R,  true,  false, false, false, false, false, false)
 )
 
-composite_cases = Dict(
-    ExponentialKernel => ([1,1], [1,0.5]),
+all_testinputs = Dict(
+    SquaredDistanceKernel   => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
+    SineSquaredKernel       => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
+    ChiSquaredKernel        => ([1,1],[1,0],[0,1],[0,0]),
+    ScalarProductKernel     => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
+    MercerSigmoidKernel     => ([1,1],[1,0],[0,1],[0,0],[-1,-1],[-1,0],[0,-1]),
+    ExponentialKernel       => ([0],[1e-10],[0.5],[1]),
+    RationalQuadraticKernel => ([0],[1e-10],[0.5],[1]),
+    MaternKernel            => ([0],[1e-10],[0.5],[1]),
+    PowerKernel             => ([0],[1e-10],[0.5],[1]),
+    LogKernel               => ([0],[1e-10],[0.5],[1]),
+    PolynomialKernel        => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1]),
+    ExponentiatedKernel     => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1]),
+    SigmoidKernel           => ([0],[1e-10],[0.5],[1],[-1e-10],[-0.5],[-1])
+
+)
+
+
+all_phicases = Dict(
+    SquaredDistanceKernel   => ([0.25],[0.5],[1]),
+    SineSquaredKernel       => ([0.25],[0.5],[1]),
+    ChiSquaredKernel        => ([0.25],[0.5],[1]),
+    ScalarProductKernel     => (Int[],),
+    MercerSigmoidKernel     => ([0.5,2],[0,1]),
+    ExponentialKernel       => ([1,1], [1,0.5]),
     RationalQuadraticKernel => ([1,1,1], [1,1,0.5], [1,2,1], [2,2,0.5]),
-    MaternKernel => ([1,1], [2,1]),
-    PowerKernel => ([0.5], [1]),
-    LogKernel => ([1,0.5], [1,1]),
-    PolynomialKernel => ([1,1,2],[1,1,1]),
-    ExponentiatedKernel => ([1],),
-    SigmoidKernel => ([1,1],)
+    MaternKernel            => ([1,1], [2,1]),
+    PowerKernel             => ([0.5], [1]),
+    LogKernel               => ([1,0.5], [1,1]),
+    PolynomialKernel        => ([1,1,2],[1,1,1]),
+    ExponentiatedKernel     => ([1],),
+    SigmoidKernel           => ([1,1],)
 )
 
-composite_errorcases = Dict(
-    ExponentialKernel => ([0,1], [1,1.1], [1,0]),
+all_errorcases = Dict(
+    SquaredDistanceKernel   => ([1.1], [0]),
+    SineSquaredKernel       => ([1.1], [0]),
+    ChiSquaredKernel        => ([1.1], [0]),
+    MercerSigmoidKernel     => ([0,0],),
+    ExponentialKernel       => ([0,1], [1,1.1], [1,0]),
     RationalQuadraticKernel => ([0,1,1], [1,0,1], [1,1,0], [1,1,1.1]),
-    MaternKernel => ([1,0], [0,1]),
-    PowerKernel => ([0], [1.1]),
-    LogKernel => ([0,1], [1,0], [1,1.1]),
-    PolynomialKernel => ([0,1,1],[1,-0.1,1],[1,1,1.5],[1,1,0]),
-    ExponentiatedKernel => ([0],),
-    SigmoidKernel => ([0,1],[1,-0.1])
+    MaternKernel            => ([1,0], [0,1]),
+    PowerKernel             => ([0], [1.1]),
+    LogKernel               => ([0,1], [1,0], [1,1.1]),
+    PolynomialKernel        => ([0,1,1],[1,-0.1,1],[1,1,1.5],[1,1,0]),
+    ExponentiatedKernel     => ([0],),
+    SigmoidKernel           => ([0,1],[1,-0.1])
 )
--- test/test_kernels.jl
+++ test/test_kernels.jl
@@ -2,42 +2,10 @@ using Base.Test
 
 importall MLKernels
 
-macro test_approx_eq_type(value, reference, typ)
-    x = gensym()
-    quote
-        $x = $value
-        @test_approx_eq $x $reference
-        @test isa($x, $typ)
-    end
-end
-
-# Check each field for equality with args (assumed same order)
-function check_fields(kernelobject::Kernel, field_values)
-    fields = names(kernelobject)
-    for i = 1:length(fields)
-        @test getfield(kernelobject, fields[i]) == field_values[i]
-    end
-end
-
-# Iterate through constructor cases - test only Float64 to ensure fields are populating as expected
-function test_constructor_case{T<:Kernel}(kernelobject::Type{T}, default_args, test_args)
-    n = length(names(kernelobject))
-    check_fields((kernelobject)(), [default_args...])
-    for i = 1:n
-        case_args = [test_args[1:i]..., default_args[(i+1):end]...]
-        κ = (kernelobject)(case_args[1:i]...)
-        check_fields(κ, case_args)
-    end
-end
-
-
-
 # Test Base Kernels
 
-for kernelobj in additive_kernels
-
+for kernelobj in (additive_kernels..., composite_kernels...)
     info("Testing ", kernelobj)
-
     for T in FloatingPointTypes
     
         if T == Float64
@@ -45,99 +13,44 @@ for kernelobj in additive_kernels
             @test eltype(k) == Float64
         end
 
-        # Test constructors
-        fields, default_args, test_args = get(additive_kernelargs, kernelobj, (Symbol[],T[],T[]))
-        for i = 1:length(fields)
-            arg_values = T[test_args[1:i]..., default_args[i+1:end]...]
-            k = (kernelobj)(arg_values...)
-            @test eltype(k) == T
-            for j = 1:length(fields)
-                @test getfield(k, fields[j]) === arg_values[j]
-            end
-        end
+        fields, default_args, test_args = get(all_kernelargs, kernelobj, (Symbol[],T[],T[]))
+        f = get(all_kernelfunctions, kernelobj, "error")
 
-        for test_args in get(additive_errorcases, kernelobj, ())
-            arg_values = T[test_args...]
-            @test_throws ErrorException (kernelobj)(arg_values...)
-        end
-
-        #Test phi() function
-        f = get(additive_kernelfunctions, kernelobj, "error")
-        for test_args in get(additive_cases, kernelobj, "error")
-            arg_values = T[test_args...]
-            k = convert(Kernel{T},(kernelobj)(arg_values...))
-            for test_inputs in get(additive_testinputs, kernelobj, "error")
-                x = convert(T, test_inputs[1])
-                y = convert(T, test_inputs[2])
-                a = MLKernels.phi(k, x, y)
-                b = f(arg_values..., x, y)
-                if T == BigFloat
-                    @test_approx_eq convert(Float64,a) convert(Float64,b)
-                else
-                    @test_approx_eq a b
-                end
-            end
-        end
-
-    end
-
-    k = (kernelobj)()
-
-    @test ismercer(k) === get(additive_ismercer, kernelobj, "error")
-    @test isnegdef(k) === get(additive_isnegdef, kernelobj, "error")
-
-
-    (kr,az,pos,nn,np,neg) = get(additive_kernelranges, kernelobj, "error")
-    @test MLKernels.kernelrange(k) == kr
-    @test attainszero(k) == az
-    @test ispositive(k) == pos
-    @test isnonnegative(k) == nn
-    @test isnonpositive(k) == np
-    @test isnegative(k) == neg
-
-end
-
-for kernelobj in composite_kernels
-
-    info("Testing ", kernelobj)
-
-    for T in FloatingPointTypes
-    
-        if T == Float64
-            k = (kernelobj)()
-            @test eltype(k) == Float64
-        end
-
-        for base_kernelobj in get(composite_pairs, kernelobj, ())
-
+        for base_kernelobj in get(composite_pairs, kernelobj, (kernelobj,))
             base_k = convert(Kernel{T}, (base_kernelobj)())
+            is_composed = kernelobj in composite_kernels
 
-            # Test constructors
-            fields, default_args, test_args = get(composite_kernelargs, kernelobj, (Symbol[],T[],T[]))
+            # Test Constructors
             for i = 1:length(fields)
-                arg_values = T[test_args[1:i]..., default_args[i+1:end]...]
-                k = (kernelobj)(base_k, arg_values...)
+                arg_values = (T[test_args[1:i]..., default_args[i+1:end]...]...)
+                k = is_composed ? (kernelobj)(base_k, arg_values...) : (kernelobj)(arg_values...)
                 @test eltype(k) == T
-                @test getfield(k, :k) === base_k
+                if is_composed
+                    @test getfield(k, :k) === base_k
+                end
                 for j = 1:length(fields)
                     @test getfield(k, fields[j]) === arg_values[j]
                 end
             end
 
-            for test_args in get(composite_errorcases, kernelobj, ())
+            # Test Error Cases
+            for test_args in get(all_errorcases, kernelobj, ())
                 arg_values = T[test_args...]
-                @test_throws ErrorException (kernelobj)(base_k, arg_values...)
+                if is_composed
+                    @test_throws ErrorException (kernelobj)(base_k, arg_values...) 
+                else
+                    @test_throws ErrorException (kernelobj)(arg_values...)
+                end
             end
 
-            # Test phi() function
-            f = get(composite_kernelfunctions, kernelobj, "error")
-            for test_args in get(composite_cases, kernelobj, "error")
+            # Test phi() function for all cases
+            for test_args in get(all_phicases, kernelobj, "error")
                 arg_values = T[test_args...]
-                k = convert(Kernel{T},(kernelobj)(base_k, arg_values...))
-                for test_input in get(composite_testinputs, kernelobj, "error")
-                    z = convert(T, test_input[1])
-                    a = MLKernels.phi(k, z)
-                    b = f(arg_values..., z)
+                k = convert(Kernel{T}, is_composed ? (kernelobj)(base_k, arg_values...) : (kernelobj)(arg_values...))
+                for test_input in get(all_testinputs, kernelobj, "error")
+                    z = T[test_input...]
+                    a = MLKernels.phi(k, z...)
+                    b = f(arg_values..., z...)
                     if T == BigFloat
                         @test_approx_eq convert(Float64,a) convert(Float64,b)
                     else
@@ -147,12 +60,20 @@ for kernelobj in composite_kernels
 
             end
 
-        end
+        end # End Base Kernel loop
+    end # End Floating Point loop
 
-    end
+    k = (kernelobj)()
 
-    @test ismercer((kernelobj)()) === get(composite_ismercer, kernelobj, "error")
-    @test isnegdef((kernelobj)()) === get(composite_isnegdef, kernelobj, "error")
+    (kr,az,pos,nn,np,neg,ismer,isnd) = get(all_kernelproperties, kernelobj, "error")
+    @test MLKernels.kernelrange(k) == kr
+    @test attainszero(k) == az
+    @test ispositive(k) == pos
+    @test isnonnegative(k) == nn
+    @test isnonpositive(k) == np
+    @test isnegative(k) == neg
+    @test ismercer(k) === ismer
+    @test isnegdef(k) === isnd
 
 end
 
