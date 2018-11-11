b2c74f8af146e88117cf11efaff046faa3d626ee
Tim Thatcher <4790109+trthatcher@users.noreply.github.com>
2018-11-11 15:53:11 -0500

    Altered formula for maternkernel - fixes #74

--- src/kernelfunctions/mercer/matern.jl
+++ src/kernelfunctions/mercer/matern.jl
@@ -4,13 +4,13 @@
 The Matern kernel is a Mercer kernel given by:
 
 ```math
-\kappa(\mathbf{x},\mathbf{y}) = 
-\frac{1}{2^{\nu-1}\Gamma(\nu)} 
-\left(\frac{2\sqrt{\nu}||\mathbf{x}-\mathbf{y}||}{\theta}\right)^{\nu} 
-K_{\nu}\left(\frac{2\sqrt{\nu}||\mathbf{x}-\mathbf{y}||}{\theta}\right)
+\kappa(\mathbf{x},\mathbf{y}) =
+\frac{1}{2^{\nu-1}\Gamma(\nu)}
+\left(\frac{\sqrt{2\nu}||\mathbf{x}-\mathbf{y}||}{\theta}\right)^{\nu}
+K_{\nu}\left(\frac{\sqrt{2\nu}||\mathbf{x}-\mathbf{y}||}{\theta}\right)
 ```
 
-where ``\Gamma`` is the gamma function, ``K_{\nu}`` is the modified Bessel function of the 
+where ``\Gamma`` is the gamma function, ``K_{\nu}`` is the modified Bessel function of the
 second kind, ``\nu > 0`` and ``\theta > 0``.
 """
 struct MaternKernel{T<:AbstractFloat} <: MercerKernel{T}
@@ -25,10 +25,11 @@ function MaternKernel(ν::T1=1.0, ρ::T2=one(T1)) where {T1<:Real,T2<:Real}
     MaternKernel{floattype(T1,T2)}(ν,ρ)
 end
 
-@inline function maternkernel(z::T, ν::T, ρ::T) where {T}
-    v1 = sqrt(2ν) * z / ρ
-    v1 = v1 < eps(T) ? eps(T) : v1  # Overflow risk as z -> Inf
-    2 * (v1/2)^(ν) * besselk(ν, v1) / gamma(ν)
+@inline function maternkernel(d²::T, ν::T, ρ::T) where {T<:AbstractFloat}
+    d = √(d²)
+    d = d < eps(T) ? eps(T) : d  # If d is zero, besselk will return NaN
+    tmp = √(2ν)d/ρ
+    (2^(1 - ν))*(tmp^ν)*besselk(ν, tmp)/gamma(ν)
 end
 
 @inline basefunction(::MaternKernel) = SquaredEuclidean()
